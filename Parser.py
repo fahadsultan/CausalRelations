9c85d15a442d3224c25f2154431991965b994f2a
Fahad <fahad@Fahads-MacBook-Pro.local>
2015-09-18 22:06:01 +0300

    Fixed a big mistake + Added the isCausalRelation column. Except for features, I think I've done everything in the parser

--- Parser.py
+++ Parser.py
@@ -10,7 +10,7 @@ class Parser:
 		self.data = []
 
 	def getEventPairs(self):
-		
+
 		for folder, subs, files in os.walk('data/xml'):
 			
 			for filename in files:
@@ -26,84 +26,106 @@ class Parser:
 						for i in range(0,len(events)-1):
 
 							event = events[i]
-							next_event = events[i+1]
 
-							event_token_id 		= event.find('token_anchor').attrs['id']
-							next_event_token_id = next_event.find('token_anchor').attrs['id']
+							for j in range(i+1, len(events)):
 
-							event_token_tag 	 =	soup.find(lambda tag: tag.name == 'token' and
-																	tag['id'] == event_token_id)
+								next_event = events[j]
 
-							next_event_token_tag = 	soup.find(lambda tag: tag.name == 'token' and
-																	tag['id'] == next_event_token_id)
+								event_token_id 		= event.find('token_anchor').attrs['id']
+								next_event_token_id = next_event.find('token_anchor').attrs['id']
 
-							event_sentence = event_token_tag['sentence']
-							next_event_sentence = next_event_token_tag['sentence']
+								event_token_tag 	 =	soup.find(lambda tag: tag.name == 'token' and
+																		tag['id'] == event_token_id)
 
-							if (int(next_event_sentence) - int(event_sentence)) <= 1:
-								
-								event_one_token_id 	= event_token_tag.attrs['id']
-								event_one_number 	= event_token_tag.attrs['number']
-								event_one_sentence 	= event_sentence
-								event_one_token 	= event_token_tag.text
-								event_one_aspect 	= event.attrs['aspect']
-								event_one_certainty = event.attrs['certainty']
-								event_one_class 	= event.attrs['class']
-								event_one_comment 	= event.attrs['comment']
-								event_one_factuality= event.attrs['factuality']
-								event_one_event_id 	= event.attrs['id']
-								event_one_modality 	= event.attrs['modality']
-								event_one_polarity 	= event.attrs['polarity']
-								event_one_pos 		= event.attrs['pos']
-								event_one_tense 	= event.attrs['tense']
-
-								event_two_token_id 	= next_event_token_tag.attrs['id']
-								event_two_number 	= next_event_token_tag.attrs['number']
-								event_two_sentence 	= event_sentence
-								event_two_token 	= next_event_token_tag.text
-								event_two_aspect 	= next_event.attrs['aspect']
-								event_two_certainty	= next_event.attrs['certainty']
-								event_two_class 	= next_event.attrs['class']
-								event_two_comment 	= next_event.attrs['comment']
-								event_two_factuality= next_event.attrs['factuality']
-								event_two_event_id 	= next_event.attrs['id']
-								event_two_modality 	= next_event.attrs['modality']
-								event_two_polarity 	= next_event.attrs['polarity']
-								event_two_pos 		= next_event.attrs['pos']
-								event_two_tense 	= next_event.attrs['tense']
-								
-								row = [	filename.split('.xml')[0],
-										event_one_token_id, 
-										event_one_number,
-										event_one_sentence,
-										event_one_token,
-										event_one_aspect, 
-										event_one_certainty,
-										event_one_class,
-										event_one_comment,
-										event_one_factuality,
-										event_one_event_id,
-										event_one_modality,
-										event_one_polarity,
-										event_one_pos,
-										event_one_tense,
-										event_two_token_id, 
-										event_two_number,
-										event_two_sentence,
-										event_two_token,
-										event_two_aspect, 
-										event_two_certainty,
-										event_two_class,
-										event_two_comment,
-										event_two_factuality,
-										event_two_event_id,
-										event_two_modality,
-										event_two_polarity,
-										event_two_pos,
-										event_two_tense  ]
+								next_event_token_tag = 	soup.find(lambda tag: tag.name == 'token' and
+																		tag['id'] == next_event_token_id)
 
 
-								self.data.append(row)
+
+								event_sentence = event_token_tag['sentence']
+								next_event_sentence = next_event_token_tag['sentence']
+
+								if (int(next_event_sentence) - int(event_sentence)) > 1:
+									
+									break #Since we are only catering for events in the current sentence and the next one 
+
+								else:
+									
+									event_one_token_id 	= event_token_tag.attrs['id']
+									event_one_number 	= event_token_tag.attrs['number']
+									event_one_sentence 	= event_sentence
+									event_one_token 	= event_token_tag.text
+									event_one_aspect 	= event.attrs['aspect']
+									event_one_certainty = event.attrs['certainty']
+									event_one_class 	= event.attrs['class']
+									event_one_comment 	= event.attrs['comment']
+									event_one_factuality= event.attrs['factuality']
+									event_one_event_id 	= event.attrs['id']
+									event_one_modality 	= event.attrs['modality']
+									event_one_polarity 	= event.attrs['polarity']
+									event_one_pos 		= event.attrs['pos']
+									event_one_tense 	= event.attrs['tense']
+
+									event_two_token_id 	= next_event_token_tag.attrs['id']
+									event_two_number 	= next_event_token_tag.attrs['number']
+									event_two_sentence 	= event_sentence
+									event_two_token 	= next_event_token_tag.text
+									event_two_aspect 	= next_event.attrs['aspect']
+									event_two_certainty	= next_event.attrs['certainty']
+									event_two_class 	= next_event.attrs['class']
+									event_two_comment 	= next_event.attrs['comment']
+									event_two_factuality= next_event.attrs['factuality']
+									event_two_event_id 	= next_event.attrs['id']
+									event_two_modality 	= next_event.attrs['modality']
+									event_two_polarity 	= next_event.attrs['polarity']
+									event_two_pos 		= next_event.attrs['pos']
+									event_two_tense 	= next_event.attrs['tense']
+									
+									causal_relation_exists = len(soup.findAll(lambda tag: 
+																tag.name == 'source' and 
+																tag.findParent().name == 'clink' and 
+																tag.findNextSibling().name == 'target' and
+
+																((tag.attrs['id'] == event_one_event_id and 
+																	tag.findNextSibling().attrs['id'] == event_two_event_id) 
+																or 
+																(tag.attrs['id'] == event_two_event_id and 
+																	tag.findNextSibling().attrs['id'] == event_one_event_id))  )) > 0
+
+
+									row = [	filename.split('.xml')[0],
+											event_one_token_id, 
+											event_one_number,
+											event_one_sentence,
+											event_one_token,
+											event_one_aspect, 
+											event_one_certainty,
+											event_one_class,
+											event_one_comment,
+											event_one_factuality,
+											event_one_event_id,
+											event_one_modality,
+											event_one_polarity,
+											event_one_pos,
+											event_one_tense,
+											event_two_token_id, 
+											event_two_number,
+											event_two_sentence,
+											event_two_token,
+											event_two_aspect, 
+											event_two_certainty,
+											event_two_class,
+											event_two_comment,
+											event_two_factuality,
+											event_two_event_id,
+											event_two_modality,
+											event_two_polarity,
+											event_two_pos,
+											event_two_tense,
+											causal_relation_exists  ]
+
+
+									self.data.append(row)
 				except Exception as e:
 					print '-----------------'
 					print e
@@ -112,12 +134,6 @@ class Parser:
 					print '-----------------'
 					continue
 
-		df = pd.DataFrame(self.data)
-
 if __name__ == "__main__":
 	parser = Parser()
 	parser.getEventPairs()
-					
-
-
-
