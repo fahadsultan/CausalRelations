4d3b1503be726c2921ac6b5bb875617fc9410e42
Fahad <fahad@Fahads-MacBook-Pro.local>
2015-09-18 19:31:05 +0300

    Good progress on the parser. Not quite done yet though.

---
+++ Parser.py
@@ -0,0 +1,123 @@
+
+from bs4 import BeautifulSoup
+import os
+import pandas as pd
+import sys
+
+class Parser:
+
+	def __init__(self):
+		self.data = []
+
+	def getEventPairs(self):
+		
+		for folder, subs, files in os.walk('data/xml'):
+			
+			for filename in files:
+				
+				try:
+					print filename
+					if ('.xml' in filename) and (filename[0] != '.'):
+						f = open(os.path.join(folder, filename))
+						soup = BeautifulSoup(f.read())
+						events = soup.findAll('event')
+						tokens = soup.findAll('token')
+						
+						for i in range(0,len(events)-1):
+
+							event = events[i]
+							next_event = events[i+1]
+
+							event_token_id 		= event.find('token_anchor').attrs['id']
+							next_event_token_id = next_event.find('token_anchor').attrs['id']
+
+							event_token_tag 	 =	soup.find(lambda tag: tag.name == 'token' and
+																	tag['id'] == event_token_id)
+
+							next_event_token_tag = 	soup.find(lambda tag: tag.name == 'token' and
+																	tag['id'] == next_event_token_id)
+
+							event_sentence = event_token_tag['sentence']
+							next_event_sentence = next_event_token_tag['sentence']
+
+							if (int(next_event_sentence) - int(event_sentence)) <= 1:
+								
+								event_one_token_id 	= event_token_tag.attrs['id']
+								event_one_number 	= event_token_tag.attrs['number']
+								event_one_sentence 	= event_sentence
+								event_one_token 	= event_token_tag.text
+								event_one_aspect 	= event.attrs['aspect']
+								event_one_certainty = event.attrs['certainty']
+								event_one_class 	= event.attrs['class']
+								event_one_comment 	= event.attrs['comment']
+								event_one_factuality= event.attrs['factuality']
+								event_one_event_id 	= event.attrs['id']
+								event_one_modality 	= event.attrs['modality']
+								event_one_polarity 	= event.attrs['polarity']
+								event_one_pos 		= event.attrs['pos']
+								event_one_tense 	= event.attrs['tense']
+
+								event_two_token_id 	= next_event_token_tag.attrs['id']
+								event_two_number 	= next_event_token_tag.attrs['number']
+								event_two_sentence 	= event_sentence
+								event_two_token 	= next_event_token_tag.text
+								event_two_aspect 	= next_event.attrs['aspect']
+								event_two_certainty	= next_event.attrs['certainty']
+								event_two_class 	= next_event.attrs['class']
+								event_two_comment 	= next_event.attrs['comment']
+								event_two_factuality= next_event.attrs['factuality']
+								event_two_event_id 	= next_event.attrs['id']
+								event_two_modality 	= next_event.attrs['modality']
+								event_two_polarity 	= next_event.attrs['polarity']
+								event_two_pos 		= next_event.attrs['pos']
+								event_two_tense 	= next_event.attrs['tense']
+								
+								row = [	filename.split('.xml')[0],
+										event_one_token_id, 
+										event_one_number,
+										event_one_sentence,
+										event_one_token,
+										event_one_aspect, 
+										event_one_certainty,
+										event_one_class,
+										event_one_comment,
+										event_one_factuality,
+										event_one_event_id,
+										event_one_modality,
+										event_one_polarity,
+										event_one_pos,
+										event_one_tense,
+										event_two_token_id, 
+										event_two_number,
+										event_two_sentence,
+										event_two_token,
+										event_two_aspect, 
+										event_two_certainty,
+										event_two_class,
+										event_two_comment,
+										event_two_factuality,
+										event_two_event_id,
+										event_two_modality,
+										event_two_polarity,
+										event_two_pos,
+										event_two_tense  ]
+
+
+								self.data.append(row)
+				except Exception as e:
+					print '-----------------'
+					print e
+					print filename
+					print sys.exc_info()[0]
+					print '-----------------'
+					continue
+
+		df = pd.DataFrame(self.data)
+
+if __name__ == "__main__":
+	parser = Parser()
+	parser.getEventPairs()
+					
+
+
+
